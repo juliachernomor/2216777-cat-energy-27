43599b7a4d9b756a02552827c41cbdb295b8cbfc
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-08-29 05:03:29 +0300

    stylelint3

--- source/js/script-bubble.js
+++ source/js/script-bubble.js
@@ -4,7 +4,7 @@ const calculateRandomValue = (e, t) => {
 
 const createAnimateBubbles = (element) => {
   let i = " ";
-  for (a = 0; a < 150; a++) {
+  for (a = 0; a < 120; a++) {
     let n = calculateRandomValue(2, 30);
     i += `<div class="bubbles__object" style="height: ${n}px; width: ${n}px; background-color: rgb(${calculateRandomValue(0, 100)},${calculateRandomValue(55, 75)},${calculateRandomValue(55, 74)}); animation-duration: ${calculateRandomValue(10, 30)}s; top: ${calculateRandomValue(0, 100)}%; left: ${calculateRandomValue(0, 100)}%;"></div>`
   }
